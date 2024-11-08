6c9ec18129b68cc506c302d6e48bcc7db37d8bf7
svnnynior <pitchayut.jr@gmail.com>
2024-11-08 21:48:41 +1100

    feat: change to page router as testing-library does not support react 19 yet

---
+++ show-me-the-money/smtm-app/src/pages/balance-sheet/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BalanceSheet() {
+  return <div>This should be the balance sheet page</div>;
+}
--- show-me-the-money/smtm-app/src/pages/index.tsx
+++ show-me-the-money/smtm-app/src/pages/index.tsx
@@ -1,10 +1,25 @@
+import Image from "next/image";
+import localFont from "next/font/local";
 import Link from "next/link";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
       <h1 className="text-4xl font-bold">💰 Show Me The Money 💰</h1>
-      <span className="text-lg mt-4">An application that shows your data from Xero.</span>
+      <span className="text-lg mt-4">
+        An application that shows your data from Xero.
+      </span>
       <Link href="/balance-sheet">
         <button className="border border-white text-white px-4 py-2 rounded-md mt-4 hover:bg-white hover:text-black">
           View Balance Sheet
